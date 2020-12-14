5e5dbfe37fba1a2b7a40d75dccd845e094560a4a
Alberto Martínez <alb.mtez@gmail.com>
2020-12-15 00:29:44 +0100

    links to config files

--- initialize.zsh
+++ initialize.zsh
@@ -1,2 +1,6 @@
 git submodule update --init --recursive
 fzf/install --no-key-bindings --no-completion --no-update-rc
+
+
+ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
